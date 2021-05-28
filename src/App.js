7d7da0db4f469336aff51e3616046ae0c133af21
dinhphu28 <18110175@student.hcmute.edu.vn>
2021-05-28 23:45:27 +0700

    Embed canva to EC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import CanvaEmbed from './components/CanvaEmbed';
+import ystyle from './components/CanvaEmbed.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CanvaEmbed/>
+
+      <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEfx0DJTbE&#x2F;view?utm_content=DAEfx0DJTbE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">0886088666</a> by Đình Phú Nguyễn
+
     </div>
   );
 }
---
+++ src/components/CanvaEmbed.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Iframe from 'react-iframe';
+import yeahmystyle from './CanvaEmbed.css';
+
+class CanvaEmbed extends Component {
+
+    render() {
+        return (
+            <div>
+                <Iframe width="100%"
+                    height="720px"
+                    allow="fullscreen"
+                    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEfx0DJTbE&#x2F;view?embed">
+                </Iframe>
+            </div>
+        );
+    }
+}
+
+
+export default CanvaEmbed;
+
+// style="position: relative; width: 100%; height: 0; padding-top: 56.2500%; padding-bottom: 48px; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;"
