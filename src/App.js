2a726c3bff54edbaf7dec5bcd174246d418fdf35
dinhphu28 <18110175@student.hcmute.edu.vn>
2021-05-29 12:42:04 +0700

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     <div>
       <CanvaEmbed/>
 
-      <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEfx0DJTbE&#x2F;view?utm_content=DAEfx0DJTbE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">0886088666</a> by Đình Phú Nguyễn
+      {/* <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEfx0DJTbE&#x2F;view?utm_content=DAEfx0DJTbE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">0886088666</a> */}
+      By Đình Phú Nguyễn
 
     </div>
   );
