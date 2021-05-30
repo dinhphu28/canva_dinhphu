d915b8c1cd2b720781695e46f9789cf9c330696e
dinhphu28 <18110175@student.hcmute.edu.vn>
2021-05-31 00:22:07 +0700

    Update Canva Embed EC

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <CanvaEmbed/>
 
       {/* <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEfx0DJTbE&#x2F;view?utm_content=DAEfx0DJTbE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">0886088666</a> */}
-      By Đình Phú Nguyễn
+      <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEf-EiuN4g&#x2F;view?utm_content=DAEf-EiuN4g&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">EC18A002</a> By Đình Phú Nguyễn
 
     </div>
   );
--- src/components/CanvaEmbed.js
+++ src/components/CanvaEmbed.js
@@ -7,10 +7,15 @@ class CanvaEmbed extends Component {
     render() {
         return (
             <div>
-                <Iframe width="100%"
+                <Iframe
+                    width="100%"
                     height="720px"
+                    // display="fixed"
+                    // top="0"
+                    // left="0"
+                    // bottom="0"
                     allow="fullscreen"
-                    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEfx0DJTbE&#x2F;view?embed">
+                    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEf-EiuN4g&#x2F;view?embed">
                 </Iframe>
             </div>
         );
